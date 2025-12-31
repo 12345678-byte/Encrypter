fc08e286fd994c4a7d58636d0960ddc6e445ccc6
Yash <yashgore0611@gmail.com>
2026-01-01 00:50:38 +0530

    Modified the code to be cleaner and efficient

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from cryptography.fernet import Fernet
 import os
-
+p = 'secret.key'
 def generate(path):
-    try:
-        if (os.path.getsize(path) == 0):
-            with open(path, 'wb') as file:
-                key = Fernet.generate_key()
-                file.write(key)
+    if not os.path.exists(path) or (os.path.getsize(path) == 0):
+        with open(path, 'wb') as file:
+                file.write(Fernet.generate_key())
                 print("Key created successfully")
-    except FileNotFoundError:
-        with open(path, 'w') as f_empty:
-            pass
-        generate(path)
+        return
+    else:
+         return
+    
+generate(p)
 
-generate('secret.key')
+with open(p, "rb") as file:
+    key = file.read()
+print(key)
